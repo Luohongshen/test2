edc0946e05976049ffa30130ed898eedca927533
北极星 <1770259775@qq.com>
2023-05-01 22:54:13 +0800

    初始化

--- src/main/java/com/example/test2/Test2Application.java
+++ src/main/java/com/example/test2/Test2Application.java
@@ -2,7 +2,7 @@ package com.example.test2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//11
 @SpringBootApplication
 public class Test2Application {
 
